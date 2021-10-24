34aba8c39d36a9db269927578c77c96fc01b5538
Anthuan Vasquez <eigthy@gmail.com>
2021-10-24 01:59:44 -0400

    Update main imports

---
+++ src/helpers/index.js
@@ -0,0 +1,8 @@
+import getGraphQLQueries from "./graphql-queries";
+import splitStringByDelimiter from './query-string';
+
+
+export default {
+    queries: getGraphQLQueries(),
+    splitString: splitStringByDelimiter
+};
